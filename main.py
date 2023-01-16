72ba004e5768b09704b556dd31966a4926a3dfc2
Kiran <vakatikiran007@gmail.com>
2023-01-16 15:19:05 +0530

    format changes

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello team-CSB-Batch-6 Our task is in-progress will update you soon the date of completion of this task details!!!'
+        return f'Hello team-CSB-Batch-6 Our task is in-progress will update you soon the date of completion of this task details in the task!!!'
