930d76d9915878e0faa09c9f0058a4b3cec0b264
Kiran <vakatikiran007@gmail.com>
2023-01-16 21:05:12 +0530

    re-created file

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello team-CSB-Batch-6 Our task is in-progress will update you soon the date of completion of this task details in the tasks of this world!!!'
+        return f'Hello team-CSB-Batch-6 welcome to our GCP World!!!'
