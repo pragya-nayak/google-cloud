1d76910c77f7ac16a4c971afe1ed135e5b441abd
kirankittu1996 <95482515+kirankittu1996@users.noreply.github.com>
2023-03-02 12:17:39 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Give me some sun shine , Give me some rain , Give me an other chance , i wanna grow-up once again!!!'
+        return f'Microservice Testing!!!'
