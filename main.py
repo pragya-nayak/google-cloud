bb264481a781566c67f625bcf4710765109f9bad
kirankittu1996 <95482515+kirankittu1996@users.noreply.github.com>
2023-03-02 12:24:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Microservice Testing!!!'
+        return f'Microservice Testing-1!!!'
